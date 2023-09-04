93256db0193b7c1e8da7bc8fe340d1cdba46dbc0
Jan Červenka <jan.cervenka@yahoo.com>
2023-09-04 10:20:46 +0200

    fix tests

--- src/epstats/prometheus.py
+++ src/epstats/prometheus.py
@@ -1,7 +1,8 @@
 from prometheus_client import Counter, Summary, REGISTRY
+from typing import Union
 
 
-def get_prometheus_metric(metric_name: str, metric_type: type) -> Counter | Summary:
+def get_prometheus_metric(metric_name: str, metric_type: type) -> Union[Counter, Summary]:
     try:
         return metric_type(metric_name, "")
     except ValueError:
