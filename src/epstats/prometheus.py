51a03b0f5ed40052e1e5bf77241460fc1c55120d
jachymsol <solejabreck@gmail.com>
2023-10-02 10:12:29 +0200

    EXP-963: Monitor evaluation performance in Prometheus

--- src/epstats/prometheus.py
+++ src/epstats/prometheus.py
@@ -5,9 +5,11 @@ from typing import Union
 application_prefix = "ep_stats_"
 
 
-def get_prometheus_metric(metric_name: str, metric_type: type) -> Union[Counter, Summary]:
+def get_prometheus_metric(metric_name: str, metric_type: type, labels: list[str] = None) -> Union[Counter, Summary]:
+    if labels is None:
+        labels = []
     metric_name_with_application_prefix = application_prefix + metric_name
     try:
-        return metric_type(metric_name_with_application_prefix, "")
+        return metric_type(metric_name_with_application_prefix, "", labels)
     except ValueError:
         return REGISTRY._names_to_collectors[metric_name_with_application_prefix]
