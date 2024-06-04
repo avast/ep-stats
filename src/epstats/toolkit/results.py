a32690d3f5a77071eae5f796fd2afd1bfc9c509d
ondraz <ondra.zahradnik@gmail.com>
2024-06-04 15:57:36 +0200

    fixing deprecations

--- src/epstats/toolkit/results.py
+++ src/epstats/toolkit/results.py
@@ -39,16 +39,18 @@ def results_long_to_wide(metrics: pd.DataFrame) -> pd.DataFrame:
 
 def _enrich_metric_id(row):
     """Auxiliary function for proper order of the metrics and statistics."""
-    if row[0] == "mean":
-        return row[2] + 0.1
-    elif row[0] == "diff":
-        return row[2] + 0.2
-    elif row[0] == "conf_int_lower":
-        return row[2] + 0.3
-    elif row[0] == "conf_int_upper":
-        return row[2] + 0.4
-    elif row[0] == "p_value":
-        return row[2] + 0.5
+    name = row.iloc[0]
+    metric = row.iloc[2]
+    if name == "mean":
+        return metric + 0.1
+    elif name == "diff":
+        return metric + 0.2
+    elif name == "conf_int_lower":
+        return metric + 0.3
+    elif name == "conf_int_upper":
+        return metric + 0.4
+    elif name == "p_value":
+        return metric + 0.5
 
 
 def format_results(
@@ -98,33 +100,17 @@ def format_results(
     columns_pvalue_format = {col: format_pval for col in columns_pvalue}
     columns_mean_format = {columns_mean[i]: format_mean[i] for i in range(len(columns_mean))}
 
-    columns_format = _merge_dictionaries(columns_pct_format, columns_pvalue_format, columns_mean_format)
+    columns_format = {**columns_pct_format, **columns_pvalue_format, **columns_mean_format}
 
     # Apply metric_value_multiplier, e.g. 1000 for RPM
     for i in range(len(columns_mean)):
         r[columns_mean[i]] = r[columns_mean[i]] * metric_mean_multipliers[i]
 
     # Apply columns formatting including colour p-value format
-    return r.style.format(columns_format).applymap(_p_value_color_format, subset=columns_pvalue)
+    return r.style.format(columns_format).map(_p_value_color_format, subset=columns_pvalue)
 
 
 def _p_value_color_format(pval):
     """Auxiliary function to set p-value color -- green or red."""
     color = "green" if pval < 0.05 else "red"
     return "color: %s" % color
-
-
-def _merge_dictionaries(*dictionaries):
-    """
-    Merge arbitrary number of dictionaries into one.
-
-    Arguments:
-        *dictionaries: arbitrary number of dictionaries
-
-    Returns:
-        one big dictionary consisting from *dictionaries
-    """
-    merged_dictionary = {}
-    for d in dictionaries:
-        merged_dictionary.update(d)
-    return merged_dictionary
