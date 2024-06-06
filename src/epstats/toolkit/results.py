6a90343c0a89cd692c132edbad7ea88ec797ffea
ondraz <ondra.zahradnik@gmail.com>
2024-06-06 09:41:15 +0200

    Fixing warnings and tests

--- src/epstats/toolkit/results.py
+++ src/epstats/toolkit/results.py
@@ -13,32 +13,33 @@ def _add_confidence_intervals(metrics: pd.DataFrame) -> pd.DataFrame:
 
 def results_long_to_wide(metrics: pd.DataFrame) -> pd.DataFrame:
     """Adjusts metric results from long format to wide."""
+    metrics = _add_confidence_intervals(metrics)
 
     # Change experiment variants to upper case
     metrics = metrics.assign(exp_variant_id=lambda r: r.exp_variant_id.str.title())
 
     # Reshape metrics DataFrame - from long to wide
-    r = metrics.pivot_table(
+    metrics = metrics.pivot_table(
         index=["exp_id", "exp_variant_id"],
         columns=["metric_name", "metric_id"],
         values=["mean", "diff", "conf_int_lower", "conf_int_upper", "p_value"],
     )
 
     # Add column multi-index names and transpose
-    r.columns.names = ["statistic", "metric_name", "metric_id"]
-    r = r.transpose()
+    metrics.columns.names = ["statistic", "metric_name", "metric_id"]
+    metrics = metrics.transpose()
 
     # Sort metrics and statistics in the right order
-    r.reset_index(inplace=True)  # noqa: PD002
-    r["metric_id"] = r.apply(_enrich_metric_id, axis="columns")
-    r.sort_values(by="metric_id", inplace=True)  # noqa: PD002
-    r.drop(columns=[("metric_id", "")], inplace=True)  # noqa: PD002
+    metrics = metrics.reset_index()
+    metrics["metric_id"] = metrics.apply(_enrich_metric_id, axis="columns")
+    metrics = metrics.sort_values(by="metric_id")
+    metrics = metrics.drop(columns=[("metric_id", "")])
 
     # Set index and transpose back
-    r.set_index(["metric_name", "statistic"], inplace=True)  # noqa: PD002
-    r = r.transpose()
+    metrics = metrics.set_index(["metric_name", "statistic"])
+    metrics = metrics.transpose()
 
-    return r
+    return metrics
 
 
 def _enrich_metric_id(row):
@@ -58,46 +59,45 @@ def _enrich_metric_id(row):
 
 
 def format_results(
-    r: pd.DataFrame, experiment: Experiment, format_pct: str = "{:+,.1%}", format_pval: str = "{:,.3f}"
+    metrics: pd.DataFrame, experiment: Experiment, format_pct: str = "{:+,.1%}", format_pval: str = "{:,.3f}"
 ) -> pd.DataFrame:
     """
-    Method formatting wide dataframe with resutls. Using params `format_pct` and `format_pval` you can set number of
+    Method formatting wide dataframe with results. Using params `format_pct` and `format_pval` you can set number of
     decimals.
 
     Arguments:
-        r: dataframe with resutls in wide format
+        metrics: dataframe with results in wide format
         experiment: evaluated experiment
         format_pct: optional param with format of columns `Impact`, `Conf. interval lower bound` and `Conf. interval
         upper bound`
         format_pval: optional param with format of `p-value` column
 
     Returns:
-        nicely formated dataframe
+        nicely formatted dataframe
     """
     # Fix ugly naming
-    r.rename(
+    metrics = metrics.rename(
         columns={
             "mean": "Mean",
             "diff": "Impact",
             "conf_int_lower": "Conf. interval lower bound",
             "conf_int_upper": "Conf. interval upper bound",
             "p_value": "p-value",
-        },
-        inplace=True,  # noqa: PD002
+        }
     )
 
     # Set names for axis
-    r.columns.names = ["Metric", "Statistics"]
-    r.index.names = ["Experiment Id", "Variant"]
+    metrics.columns.names = ["Metric", "Statistics"]
+    metrics.index.names = ["Experiment Id", "Variant"]
 
-    # How should mean (metric value) formated
+    # How should mean (metric value) formatted
     format_mean = [metric.metric_format for metric in experiment.metrics]
     metric_mean_multipliers = [metric.metric_value_multiplier for metric in experiment.metrics]
 
     # Select appropriate columns
-    columns_pct = [col for col in r.columns if ("interval" in col[1]) | ("Impact" in col[1])]
-    columns_pvalue = [col for col in r.columns if "p-value" in col[1]]
-    columns_mean = [col for col in r.columns if "Mean" in col[1]]
+    columns_pct = [col for col in metrics.columns if ("interval" in col[1]) | ("Impact" in col[1])]
+    columns_pvalue = [col for col in metrics.columns if "p-value" in col[1]]
+    columns_mean = [col for col in metrics.columns if "Mean" in col[1]]
 
     # Set formatting for specific columns
     columns_pct_format = {col: format_pct for col in columns_pct}
@@ -108,10 +108,10 @@ def format_results(
 
     # Apply metric_value_multiplier, e.g. 1000 for RPM
     for i in range(len(columns_mean)):
-        r[columns_mean[i]] = r[columns_mean[i]] * metric_mean_multipliers[i]
+        metrics[columns_mean[i]] = metrics[columns_mean[i]] * metric_mean_multipliers[i]
 
     # Apply columns formatting including colour p-value format
-    return r.style.format(columns_format).map(_p_value_color_format, subset=columns_pvalue)
+    return metrics.style.format(columns_format).map(_p_value_color_format, subset=columns_pvalue)
 
 
 def format_metrics_long(
